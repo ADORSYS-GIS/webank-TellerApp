704395b3f37d85bfe3329fb559fa8c5a9eedafba
arielpetit <Ariel.Tchikaya@adorsys.com>
2024-09-23 15:24:57 +0100

    feat : setup the frontend architecture for the Webank TellerApp

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+
+      <h1><center>WEBANK TELLER-APP</center></h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'webank-TellerApp',
+      short_name: 'webank-TellerApp',
+      description: 'A PWA application for the teller ',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
